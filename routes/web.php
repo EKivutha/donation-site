385390ce8d3a474204d1806c3e64406e9941dbed
Ezra <gerkibz@outlook.com>
2022-06-17 18:17:10 +0300

    upadte app routes

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,20 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/donate', function () {
+    return Inertia::render('Donate');
+})->middleware(['auth', 'verified'])->name('donate');
+
+Route::get('/transaction', function () {
+    return Inertia::render('Transaction');
+})->middleware(['auth', 'verified'])->name('transaction');
+
+Route::get('/pesapal', function () {
+    return Inertia::render('Merchant');
+})->middleware(['auth', 'verified'])->name('pesapal');
+
+
 Route::post('/get-token', [PesaPalController::class, 'getAccessToken']);
-Route::get('/lipa', [PesaPalController::class, 'lipaNaPesaPal']);
+Route::post('/lipa', [PesaPalController::class, 'requestLipaNaPesaPal']);
 
 require __DIR__.'/auth.php';
